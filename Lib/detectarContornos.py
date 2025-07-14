600311a6d0d40649445dbc93e28dac3f6cbb1752
miguelvalero <miguel.valero@upc.edu>
2025-07-14 10:16:00 +0200

    codigos objetos

--- Lib/detectarContornos.py
+++ Lib/detectarContornos.py
@@ -2,17 +2,18 @@ import cv2 as cv
 import numpy as np
 
 
-img = cv.imread ('imagenes/bolasDiferentesMedidas.png')
+#img = cv.imread ('imagenes/bolasDiferentesMedidas.png')
+img = cv.imread ('imagenes/muchasPelotas.jpg')
 img = cv.resize(img, (720,480))
 cv.imshow ("img", img)
 hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
-lower = np.array ([28,100,100])
-upper = np.array ([32,255,255])
+lower = np.array ([25,100,100])
+upper = np.array ([35,255,255])
 
 mask = cv.inRange (hsv,lower,upper)
 kernel = np.ones((5,5), np.uint8)
-erosion = cv.erode (mask, kernel, iterations = 2)
-erosion_dilate = cv.dilate (erosion, kernel, iterations = 3)
+erosion = cv.erode (mask, kernel, iterations = 1)
+erosion_dilate = cv.dilate (erosion, kernel, iterations = 5)
 cv.imshow ("mask", mask)
 cv.imshow ("erosion", erosion)
 cv.imshow ("erosion_dilate", erosion_dilate)
