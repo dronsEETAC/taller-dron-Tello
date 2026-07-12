38033f2d9a9f09d2ff025480d6fb2a746d7ebf34
dronsEETAC <100842082+dronsEETAC@users.noreply.github.com>
2026-07-12 12:41:25 +0200

    Update video.py

--- Lib/video.py
+++ Lib/video.py
@@ -1,19 +1,19 @@
 from djitellopy import Tello
-import cv2 as cv
+import cv2
 
 tello = Tello()
 tello.connect()
 print(tello.get_battery())
 tello.streamon()
-cap = cv.VideoCapture(0)
+cap = cv2.VideoCapture(0)
 
 while True:
     telloFrame = tello.get_frame_read().frame
     _, computerFrame = cap.read()
-    telloFrame = cv.resize(telloFrame, (360, 240))
-    frame_rgb = cv.cvtColor(telloFrame, cv.COLOR_BGR2RGB)
-    cv.imshow("tello",  frame_rgb)
-    cv.waitKey(1)
-    computerFrame = cv.resize(computerFrame, (360, 240))
-    cv.imshow("computer", computerFrame)
-    cv.waitKey(1)
+    telloFrame = cv2.resize(telloFrame, (360, 240))
+    frame_rgb = cv2.cvtColor(telloFrame, cv2.COLOR_BGR2RGB)
+    cv2.imshow("tello",  frame_rgb)
+    cv2.waitKey(1)
+    computerFrame = cv2.resize(computerFrame, (360, 240))
+    cv2.imshow("computer", computerFrame)
+    cv2.waitKey(1)
